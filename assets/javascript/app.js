bf41fff98901b26480a283e6b9a7a3ba95849568
EFerri222 <eferri222@gmail.com>
2018-12-09 20:00:10 -0500

    updated html, css, and javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     $("#quiz").hide();
+    $("#results").hide();
 });
 
 function start() {
@@ -8,5 +9,49 @@ function start() {
 }
 
 function done() {
-    alert("Done!");
+    var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+    if (question1 === "Made the Kessel Run in 12 Parsecs") {
+        correct++;
+    } else if (question1 === "Luke" || question1 === "C3PO" || question1 === "Blah") {
+        incorrect++;
+    } else {
+        unanswered++;
+    }
+
+    if (question2 === "Vader") {
+        correct++;
+    } else if (question2 === "Luke" || question2 === "C3PO" || question2 === "Blah") {
+        incorrect++;
+    } else {
+        unanswered++;
+    }
+
+    if (question1 === "Vader") {
+        correct++;
+    } else if (question1 === "Luke" || question1 === "C3PO" || question1 === "Blah") {
+        incorrect++;
+    } else {
+        unanswered++;
+    }
+
+    if (question1 === "Vader") {
+        correct++;
+    } else if (question1 === "Luke" || question1 === "C3PO" || question1 === "Blah") {
+        incorrect++;
+    } else {
+        unanswered++;
+    }
+
+    $("#quiz").hide();
+    $("#results").show();
+    $("#number-correct").text("Correct: " + correct);
+    $("#number-incorrect").text("Incorrect: " + incorrect);
+    $("#number-unanswered").text("Unanswered: " + unanswered);
 }
